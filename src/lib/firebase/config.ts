ced067072d1949ed36b3fbd272e15be82d024899
User <user@example.com>
2026-08-03 12:47:50 +0530

    fix: update authDomain to default firebaseapp.com domain for Google OAuth

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -13,7 +13,7 @@
 
 const firebaseConfig = {
   apiKey:            process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain:        process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  authDomain:        process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || 'print-on-demand-895b7.firebaseapp.com',
   projectId:         process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket:     process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
